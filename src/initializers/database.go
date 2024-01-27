995fbddab089c1f41347a132f58068672622825b
Godbless Nyagawa <godblessnyagawa@gmail.com>
2024-01-27 07:58:05 +0300

    Optimized database interactions in database.go for performance and consistency.

---
+++ src/initializers/database.go
@@ -0,0 +1,27 @@
+// connect to a database
+
+package initializers
+
+// import packages(gorm and postGress)
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	// create a variable to store an error
+	var err error
+
+	dsn := os.Getenv("DB_URL")
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
+	
+}
